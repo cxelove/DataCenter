1190b8ed6d316cf09aad828bb68bc50e81754947
cxelove <chenxianen@vip.qq.com>
2020-06-29 08:13:40 +0800

    6.28

--- src/main/java/com/ldchina/datacenter/controller/ExportController.java
+++ src/main/java/com/ldchina/datacenter/controller/ExportController.java
@@ -6,6 +6,8 @@ import com.ldchina.datacenter.sensor.ChannelInfo;
 import com.ldchina.datacenter.types.Layui;
 import com.ldchina.datacenter.utils.DbUtil;
 import com.ldchina.datacenter.utils.TimeUtil;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.http.HttpHeaders;
@@ -36,129 +38,84 @@ public class ExportController {
         return mav;
     }
 
-//    // 自适应宽度(中文支持)
-//    private void setSizeColumn(XSSFSheet sheet, int size) {
-//        for (int columnNum = 0; columnNum < size; columnNum++) {
-//            int columnWidth = sheet.getColumnWidth(columnNum) / 256;
-//            for (int rowNum = 0; rowNum < sheet.getLastRowNum(); rowNum++) {
-//                XSSFRow currentRow;
-//                //当前行未被使用过
-//                if (sheet.getRow(rowNum) == null) {
-//                    currentRow = sheet.createRow(rowNum);
-//                } else {
-//                    currentRow = sheet.getRow(rowNum);
-//                }
-//
-//                if (currentRow.getCell(columnNum) != null) {
-//                    XSSFCell currentCell = currentRow.getCell(columnNum);
-//                    if (currentCell.getCellType() == XSSFCell.CELL_TYPE_STRING) {
-//                        int length = currentCell.getStringCellValue().getBytes().length;
-//                        if (columnWidth < length) {
-//                            columnWidth = length;
-//                        }
-//                    }
-//                }
-//            }
-//            sheet.setColumnWidth(columnNum, columnWidth * 256);
-//        }
-//    }
+    // 自适应宽度(中文支持)
+    private void setSizeColumn(XSSFSheet sheet, int size) {
+        for (int columnNum = 0; columnNum < size; columnNum++) {
+            int columnWidth = sheet.getColumnWidth(columnNum) / 256;
+            for (int rowNum = 0; rowNum < sheet.getLastRowNum(); rowNum++) {
+                XSSFRow currentRow;
+                //当前行未被使用过
+                if (sheet.getRow(rowNum) == null) {
+                    currentRow = sheet.createRow(rowNum);
+                } else {
+                    currentRow = sheet.getRow(rowNum);
+                }
+
+                if (currentRow.getCell(columnNum) != null) {
+                    XSSFCell currentCell = currentRow.getCell(columnNum);
+                    if (currentCell.getCellType() == XSSFCell.CELL_TYPE_STRING) {
+                        int length = currentCell.getStringCellValue().getBytes().length;
+                        if (columnWidth < length) {
+                            columnWidth = length;
+                        }
+                    }
+                }
+            }
+            sheet.setColumnWidth(columnNum, columnWidth * 256);
+        }
+    }
 
     @RequestMapping("/download")
     public ResponseEntity<byte[]> download(String stationId, String date) {
         XSSFWorkbook workbook = new XSSFWorkbook();
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         XSSFSheet sheet = workbook.createSheet();
-        //   Qx = new QxData();
         try {
             if (DbUtil.dbMapperUtil.iSqlMapper.isTableExit("DATA_" + stationId)) {
                 String sqlString = "SELECT * FROM DATA_" + stationId + " WHERE DATEDIFF(day,obTime,'" + date
                         + "')=0 ORDER BY obTime DESC";
                 List<Map<String, Object>> resultList = DbUtil.dbMapperUtil.iSqlMapper.sqlget(sqlString);
-
                 for (int i = 0; i < resultList.size() + 1; i++) {
                     sheet.createRow(i);
                 }
                 sheet.getRow(0).createCell(0).setCellValue("观测时间");
                 sheet.getRow(0).createCell(1).setCellValue("电压(V)");
-                sheet.autoSizeColumn((short) 0);
-                sheet.autoSizeColumn((short) 1);
                 for (int n = 0; n < resultList.size(); n++) {
                     sheet.getRow(n + 1).createCell(0).setCellValue(TimeUtil.format((Date) resultList.get(n).get("OBTIME"), "yyyy-MM-dd hh:mm"));
+                    sheet.autoSizeColumn((short) 0);
                     sheet.getRow(n + 1).createCell(1).setCellValue(Float.parseFloat((String) resultList.get(n).get("PS")));
+                    sheet.autoSizeColumn((short) 1);
                 }
                 int cell = 2;
                 for (Map.Entry<String, WebConfig> webConfigEntry : AppConfig.keyToWebconfigByStationid.get(stationId).entrySet()) {
                     String mainKey = webConfigEntry.getKey().split("_")[1];
                     String subKey = webConfigEntry.getKey().split("_")[2];
                     ChannelInfo channelInfo = AppConfig.keyMainSubToChannelInfoByProtocol
-                            .get(AppConfig.stationidTostationStatus.get(stationId))
+                            .get(AppConfig.stationidTostationStatus.get(stationId).stationInfo.protocol)
                             .get(mainKey)
                             .get(subKey);
-                    if (channelInfo.unit==null) {
+                    if (channelInfo.unit == null) {
                         sheet.getRow(0).createCell(cell).setCellValue(channelInfo.name);
                     } else {
                         sheet.getRow(0).createCell(cell).setCellValue(channelInfo.name + "(" + channelInfo.unit + ")");
                     }
+                    for (int n = 0; n < resultList.size(); n++) {
+                        try {
+                            sheet.getRow(n + 1).createCell(cell).setCellValue(Double.parseDouble((String) resultList.get(n).get(channelInfo.key)));
+                        } catch (Exception e) {
+                            sheet.getRow(n + 1).createCell(cell).setCellValue((String) resultList.get(n).get(channelInfo.key));
+                        }
+                    }
                     sheet.autoSizeColumn((short) cell);
-
-
+                    setSizeColumn(sheet, cell);
+                    cell++;
                 }
-
-
+                workbook.write(baos);
             } else {
-
+                System.out.println("Data Table is NOT Exist.");
             }
-
-
-            //      qxData.setObtime(new SimpleDateFormat("yyyy-MM-dd").parse(date));
-            //     qxData.setStationid(stationId);
-//            List<QxData> qxDatas = DbUtil.dbMapperUtil.iSqlMapper.sqlget(xData);
-//            List<JsQxData> jsQxDatas = new ArrayList<>();
-//            for (int i = 0; i < qxDatas.size(); i++) {
-//                jsQxDatas.add(i, qxDatas.get(i).toJsData());
-//            }
-//            for (int i = 0; i < qxDatas.size() + 1; i++) {
-//                sheet.createRow(i);
-//            }
-            //           sheet.getRow(0).createCell(0).setCellValue("观测时间");
-//            sheet.getRow(0).createCell(1).setCellValue("电压(v)");
-//            sheet.autoSizeColumn((short) 0);
-//            sheet.autoSizeColumn((short) 1);
-//            for (int n = 0; n < jsQxDatas.size(); n++) {
-//                sheet.getRow(n + 1).createCell(0).setCellValue(jsQxDatas.get(n).obtime);
-//                sheet.getRow(n + 1).createCell(1).setCellValue(Float.parseFloat(jsQxDatas.get(n).ps));
-//            }
-            int cell = 2;
-//            for (int i = 0; i < appConfig.webConfigMapKeyList.size(); i++) {
-//                WebConfig webConfig = appConfig.webConfigMap.get(appConfig.webConfigMapKeyList.get(i));
-//                if (webConfig.getListDisplay()) {
-//                    //设置表头
-//                    if (webConfig.getUnit().equals("")) {
-//                        sheet.getRow(0).createCell(cell).setCellValue(webConfig.getRemark());
-//                    } else {
-//                        sheet.getRow(0).createCell(cell).setCellValue(webConfig.getRemark() + "(" + webConfig.getUnit() + ")");
-//                    }
-//                    sheet.autoSizeColumn((short) cell);
-//                    //设置一列数据
-//                    for (int n = 0; n < jsQxDatas.size(); n++) {
-//                        Field field = JsQxData.class.getDeclaredField(webConfig.getId().toLowerCase());
-//                        //设置对象的访问权限，保证对private的属性的访问
-//                        field.setAccessible(true);
-//                        String val = (String) field.get(jsQxDatas.get(n));
-//
-//                        try {
-//                            sheet.getRow(n + 1).createCell(cell).setCellValue(Double.parseDouble(val));
-//                        } catch (Exception e) {
-//                            sheet.getRow(n + 1).createCell(cell).setCellValue(val);
-//                        }
-//                    }
-//                    cell++;
-//                }
-//            }
-//            setSizeColumn(sheet, cell - 1);
-            workbook.write(baos);
-        } catch (Exception e) {
-            e.printStackTrace();
+        }catch (Exception ex) {
+            ex.printStackTrace();
         }
         String filename = stationId + "_" + AppConfig.stationidTostationStatus.get(stationId).stationInfo.alias + "_" + date + ".xls";
         HttpHeaders headers = new HttpHeaders();
